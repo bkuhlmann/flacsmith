036b781cc953b4e008f61e40c824d12964b11b96
Brooke Kuhlmann <brooke@alchemists.io>
2016-12-11 15:46:07 -0700

    Added FLAC album converter support.
    - Provides support for converting an entire album into FLAC.

---
+++ lib/flacsmith/converters/album.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "pathname"
+
+module Flacsmith
+  module Converters
+    # An album converter for converting an album of files into FLAC files.
+    class Album
+      FILE_EXTENSIONS = /\.(aif|aiff)/
+
+      def initialize input_dir:, output_dir: "", picture_path: "", track_converter: Track
+        @input_dir = Pathname String(input_dir)
+        @output_dir = Pathname String(output_dir)
+        @picture_path = Pathname String(picture_path)
+        @track_converter = track_converter
+      end
+
+      def convert
+        input_dir.entries.each do |path|
+          next unless path.extname =~ FILE_EXTENSIONS
+
+          track_converter.new(
+            input_path: File.join(input_dir, path),
+            picture_path: picture_path,
+            output_dir: output_dir
+          ).convert
+        end
+      end
+
+      private
+
+      attr_reader :input_dir, :output_dir, :picture_path, :track_converter
+    end
+  end
+end
---
+++ spec/flacsmith/converters/album_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Flacsmith::Converters::Album, :temp_dir do
+  let(:input_dir) { File.join Bundler.root, "spec", "support", "files" }
+  let(:picture_path) { File.join Bundler.root, "spec", "support", "files", "sample.jpg" }
+  let(:output_dir) { temp_dir }
+  subject do
+    described_class.new input_dir: input_dir, output_dir: output_dir, picture_path: picture_path
+  end
+
+  describe "#convert" do
+    it "converts input files to FLAC files" do
+      subject.convert
+
+      expect(Pathname(temp_dir).entries).to contain_exactly(
+        Pathname("."),
+        Pathname(".."),
+        Pathname("sample.flac")
+      )
+    end
+  end
+end
