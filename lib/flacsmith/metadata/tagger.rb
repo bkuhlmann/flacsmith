75e63833055669343fbb4b83cc201718b36d108a
Brooke Kuhlmann <brooke@alchemists.io>
2017-01-29 11:45:49 -0700

    Added FLAC metadata tagger.
    - Responsible for managing metadata comment tags.
    - [Details](https://www.xiph.org/vorbis/doc/v-comment.html).

---
+++ lib/flacsmith/metadata/tagger.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Flacsmith
+  module Metadata
+    class Tagger
+      def initialize path
+        @path = path
+      end
+
+      def read tags: Tag.defaults
+        options = Array(tags).map { |tag| "--show-tag=#{tag.key}" }
+        Formatter.new(`metaflac #{options.join " "} "#{path}" 2> /dev/null`).to_h
+      end
+
+      def add tags: []
+        options = Array(tags).map { |tag| "--set-tag=#{tag}" }
+        Formatter.new(`metaflac #{options.join " "} "#{path}" 2> /dev/null`).to_h
+      end
+
+      def remove tags: []
+        options = Array(tags).map { |tag| "--remove-tag=#{tag.key}" }
+        Formatter.new(`metaflac #{options.join " "} "#{path}" 2> /dev/null`).to_h
+      end
+
+      def update tags: []
+        remove tags: tags
+        add tags: tags
+      end
+
+      private
+
+      attr_reader :path
+    end
+  end
+end
---
+++ spec/flacsmith/metadata/tagger_spec.rb
@@ -0,0 +1,239 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Flacsmith::Metadata::Tagger, :temp_dir do
+  let(:album_path) { Pathname.new "#{temp_dir}/Test Artist/Test Album" }
+  let(:sample_file) { Pathname.new "#{Dir.pwd}/spec/support/files/sample.flac" }
+  let(:test_file) { Pathname.new "#{album_path}/01 Test Title.flac" }
+
+  let :default_hash do
+    {
+      "ALBUM" => "Flacsmith Album",
+      "ARTIST" => "Flacsmith Artist",
+      "CONTACT" => "Flacsmith Contact",
+      "COPYRIGHT" => "2014",
+      "DATE" => "2014-01-01",
+      "DESCRIPTION" => "Flacsmith Description",
+      "GENRE" => "Flacsmith Genre",
+      "ISRC" => "FS-F22-14-00002",
+      "LICENSE" => "MIT",
+      "LOCATION" => "Flacsmith Location",
+      "ORGANIZATION" => "Flacsmith Organization",
+      "PERFORMER" => "Flacsmith Performer",
+      "TITLE" => "Flacsmith Title",
+      "TRACKNUMBER" => "02",
+      "VERSION" => "1.0.0"
+    }
+  end
+
+  let :default_tags do
+    default_hash.map { |key, value| Flacsmith::Metadata::Tag.for key, value }
+  end
+
+  subject { described_class.new test_file }
+
+  before do
+    FileUtils.mkdir_p album_path
+    FileUtils.cp sample_file, test_file
+  end
+
+  describe "#read" do
+    context "with default tags" do
+      it "answers default tag hash" do
+        expect(subject.read).to eq(default_hash)
+      end
+    end
+
+    context "with custom tag" do
+      let(:tag) { Flacsmith::Metadata::Tag.for "VERSION" }
+
+      it "answers single tag hash" do
+        expect(subject.read(tags: tag)).to eq("VERSION" => "1.0.0")
+      end
+    end
+
+    context "with custom tags" do
+      let :tags do
+        [
+          Flacsmith::Metadata::Tag.for("COPYRIGHT"),
+          Flacsmith::Metadata::Tag.for("LICENSE")
+        ]
+      end
+
+      it "answers multiple tag hash" do
+        expect(subject.read(tags: tags)).to eq(
+          "COPYRIGHT" => "2014",
+          "LICENSE" => "MIT"
+        )
+      end
+    end
+
+    context "with nil tags" do
+      it "answers empty hash" do
+        expect(subject.read(tags: nil)).to eq({})
+      end
+    end
+
+    context "with empty tags" do
+      it "answers empty hash" do
+        expect(subject.read(tags: [])).to eq({})
+      end
+    end
+  end
+
+  describe "#add" do
+    context "with default tags" do
+      it "answers empty hash", :aggregate_failures do
+        expect(subject.add).to eq({})
+        expect(subject.read).to eq(default_hash)
+      end
+    end
+
+    context "with custom tag" do
+      let(:tag) { Flacsmith::Metadata::Tag.for "ARTIST", "Test" }
+      let(:parameters) { {tags: tag} }
+
+      it "answers single tag hash", :aggregate_failures do
+        expect(subject.add(parameters)).to eq({})
+        expect(subject.read(parameters)).to eq("ARTIST" => "Test")
+      end
+    end
+
+    context "with custom tags" do
+      let :tags do
+        [
+          Flacsmith::Metadata::Tag.for("ARTIST", "Test"),
+          Flacsmith::Metadata::Tag.for("ALBUM", "The Album")
+        ]
+      end
+
+      let(:parameters) { {tags: tags} }
+
+      it "answers multiple tag hash", :aggregate_failures do
+        expect(subject.add(parameters)).to eq({})
+        expect(subject.read(parameters)).to eq(
+          "ARTIST" => "Test",
+          "ALBUM" => "The Album"
+        )
+      end
+    end
+
+    context "with nil tags" do
+      let(:parameters) { {tags: nil} }
+
+      it "answers empty hash", :aggregate_failures do
+        expect(subject.add(parameters)).to eq({})
+        expect(subject.read(parameters)).to eq({})
+      end
+    end
+
+    context "with empty tags" do
+      let(:parameters) { {tags: []} }
+
+      it "answers empty hash", :aggregate_failures do
+        expect(subject.add(parameters)).to eq({})
+        expect(subject.read(parameters)).to eq({})
+      end
+    end
+  end
+
+  describe "#remove" do
+    context "with default tags" do
+      it "answers empty hash", :aggregate_failures do
+        expect(subject.remove).to eq({})
+        expect(subject.read).to eq(default_hash)
+      end
+    end
+
+    context "with custom tag" do
+      let(:tag) { Flacsmith::Metadata::Tag.for "ALBUM" }
+      let(:parameters) { {tags: tag} }
+
+      it "answers empty hash", :aggregate_failures do
+        expect(subject.remove(parameters)).to eq({})
+        expect(subject.read(parameters)).to eq({})
+      end
+    end
+
+    context "with custom tags" do
+      let :tags do
+        [
+          Flacsmith::Metadata::Tag.for("ARTIST"),
+          Flacsmith::Metadata::Tag.for("ALBUM")
+        ]
+      end
+
+      let(:parameters) { {tags: tags} }
+
+      it "answers empty hash", :aggregate_failures do
+        expect(subject.remove(parameters)).to eq({})
+        expect(subject.read(parameters)).to eq({})
+      end
+    end
+
+    context "with nil tags" do
+      let(:parameters) { {tags: nil} }
+
+      it "answers empty hash", :aggregate_failures do
+        expect(subject.remove(parameters)).to eq({})
+        expect(subject.read(parameters)).to eq({})
+      end
+    end
+
+    context "with empty tags" do
+      let(:parameters) { {tags: []} }
+
+      it "answers empty hash", :aggregate_failures do
+        expect(subject.remove(parameters)).to eq({})
+        expect(subject.read(parameters)).to eq({})
+      end
+    end
+  end
+
+  describe "#update" do
+    context "with default tags" do
+      it "answers empty tags", :aggregate_failures do
+        expect(subject.update).to eq({})
+        expect(subject.read).to eq(default_hash)
+      end
+    end
+
+    context "with custom tags" do
+      let :tags do
+        [
+          Flacsmith::Metadata::Tag.for("ARTIST", "Test"),
+          Flacsmith::Metadata::Tag.for("ALBUM", "The Album")
+        ]
+      end
+
+      let(:parameters) { {tags: tags} }
+
+      it "answers multiple tag hash", :aggregate_failures do
+        expect(subject.update(parameters)).to eq({})
+        expect(subject.read(parameters)).to eq(
+          "ARTIST" => "Test",
+          "ALBUM" => "The Album"
+        )
+      end
+    end
+
+    context "with nil tags" do
+      let(:parameters) { {tags: nil} }
+
+      it "answers empty hash", :aggregate_failures do
+        expect(subject.update(parameters)).to eq({})
+        expect(subject.read(parameters)).to eq({})
+      end
+    end
+
+    context "with empty tags" do
+      let(:parameters) { {tags: []} }
+
+      it "answers empty hash", :aggregate_failures do
+        expect(subject.update(parameters)).to eq({})
+        expect(subject.read(parameters)).to eq({})
+      end
+    end
+  end
+end
